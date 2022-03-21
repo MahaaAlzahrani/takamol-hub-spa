61ef07abbe2f017554b8ebc1eb3ad3c9dd619097
mahazz <mm.alzahrani@takamol.com.sa>
2022-03-21 10:19:25 +0300

    Add a header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+    <img alt="Vue logo" src="../assets/takamol-hub-w-logo.png" height="50">
+    <p>My Vue.js Application</p>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+    export default {
+        name: 'app-header',
+    }
+</script>
+
+<style lang="scss">
+
+</style>
