f166b5768b23af1a2da30f454223b64e49800179
mahazz <mm.alzahrani@takamol.com.sa>
2022-03-22 14:14:30 +0300

    Add the links of navigation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,12 @@
 <template>
-    <nav class="px-2 sm:px-4 py-2.5">
-      <div class="container flex flex-wrap justify-between items-center mx-auto">
-         <img class="mr-7 w-36 h-16" alt="Takamol Hub logo" src="../assets/Takamol_hub_logo.png">
+    <nav class="absolute w-full px-2 sm:px-4 py-2.5 z-30">
+      <div class="container flex flex-wrap justify-between items-center mx-auto bg-transparent">
+         <img class="mr-7 w-46 h-16" alt="Takamol Hub logo" src="../assets/takamol-hub-w-logo.png">
            <ul class="list-reset flex-grow lg:flex justify-end flex-1 items-center">
-              <li class="mr-3"><router-link to="/" class="inline-block hover:text-blue">About</router-link></li>
-              <li class="mr-3"><router-link to="/" class="inline-block hover:text-blue">Docs</router-link></li>
-              <li class="mr-3"><router-link to="/" class="inline-block hover:text-blue">APIs Hub</router-link></li>
-              <li class="mr-3"><router-link to="/sign-in" class="inline-block hover:text-blue">Sign in</router-link></li>
+              <li class="mr-3 text-white"><router-link to="/" class="inline-block text-white">About</router-link></li>
+              <li class="mr-3"><router-link to="/" class="inline-block text-white">Docs</router-link></li>
+              <li class="mr-3"><router-link to="/" class="inline-block text-white">APIs Hub</router-link></li>
+              <li class="mr-3"><router-link to="/sign-in" class="inline-block text-white">Sign in</router-link></li>
               <button class="btn primary-btn">Sign up</button>
              </ul>
       </div>
@@ -20,28 +20,28 @@
 </script>
 
 <style lang="scss">
-  header {
-    display: flex;
-    padding: .5rem 1rem;
+  // header {
+  //   display: flex;
+  //   padding: .5rem 1rem;
 
-    p {
-      margin-left: 1rem;
-    }
-  }
+  //   p {
+  //     margin-left: 1rem;
+  //   }
+  // }
 
-  nav {
-    margin-left: auto;
+  // nav {
+  //   margin-left: auto;
 
-    ul {
-      list-style: none;
-    }
+  //   ul {
+  //     list-style: none;
+  //   }
 
-    ul li {
-      display: inline-flex;
-      margin-left: 1rem;
-      font-size: 19px;
-    }
-  }
+  //   ul li {
+  //     display: inline-flex;
+  //     margin-left: 1rem;
+  //     font-size: 19px;
+  //   }
+  // }
   .btn {
     padding: 10px 20px;
     font-size: 19px;
