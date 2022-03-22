ad759745638a928d470e506dc0b71e05fe97487b
mahazz <mm.alzahrani@takamol.com.sa>
2022-03-22 14:46:45 +0300

    Increase mt to 7

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,10 +3,10 @@
       <div class="container flex flex-wrap justify-between items-center mx-auto bg-transparent">
          <img class="mr-7 w-46 h-16" alt="Takamol Hub logo" src="../assets/takamol-hub-w-logo.png">
            <ul class="list-reset flex-grow lg:flex justify-end flex-1 items-center">
-              <li class="mr-3"><router-link to="/" class="inline-block text-white">About</router-link></li>
-              <li class="mr-3"><router-link to="/" class="inline-block text-white">Docs</router-link></li>
-              <li class="mr-3"><router-link to="/" class="inline-block text-white">APIs Hub</router-link></li>
-              <li class="mr-3"><router-link to="/sign-in" class="inline-block text-white">Sign in</router-link></li>
+              <li class="mr-7"><router-link to="/" class="inline-block text-white">About</router-link></li>
+              <li class="mr-7"><router-link to="/" class="inline-block text-white">Docs</router-link></li>
+              <li class="mr-7"><router-link to="/" class="inline-block text-white">APIs Hub</router-link></li>
+              <li class="mr-7"><router-link to="/sign-in" class="inline-block text-white">Sign in</router-link></li>
               <button class="btn primary-btn">Sign up</button>
              </ul>
       </div>
