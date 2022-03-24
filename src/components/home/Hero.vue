d2140729bd48b52cf31b2471af58012291198cef
mahazz <mm.alzahrani@takamol.com.sa>
2022-03-24 14:05:19 +0300

    Remove white space between header and a main banner text

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -2,7 +2,7 @@
 <div class="w-hero">
   <div class="relative flex justify-center items-center md:justify-start">
         <img class="md:block w-full " :src="require('@/assets/img/background-banner.png')" alt="banner_background" />
-    <div class="flex absolute justify-start flex-col md:flex-row items-center md:space-y-12 md:space-y-0 px-2 sm:px-4">
+    <div class="flex absolute top-44 justify-start flex-col md:flex-row items-center md:space-y-12 md:space-y-0 px-2 sm:px-4">
         <div class="lg:w-2/4 custom sm:mt-96 md:mt-0 h-full flex px-4 md:px-0 z-10 justify-center items-center md:items-start flex-col md:pl-20 lg:px-20 2xl:px-44">
             <p class="text-xl sm:text-2xl xl:text-4xl text-center md:text-left leading-6 xl:leading-10 text-gray-100 md:w-96 2xl:w-2/3" >Takamol Hub</p>
             <p class="mt-4 md:w-80 lg:w-2/3 xl:w-3/4 text-center md:text-left  text-base leading-normal text-gray-200" >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</p>
@@ -15,7 +15,7 @@
             </button> 
             </div>  
         </div> 
-        <img class="lg:w-2/4 mr-14" src="@/assets/img/image_1.png" />       
+        <img class="lg:w-2/4 mr-14 pt-20" src="@/assets/img/image_1.png" />       
     </div>
 </div>
 </div>
