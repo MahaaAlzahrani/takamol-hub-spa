ef84922777e4357f0e22f35e8ee8571d25f10e87
mahazz <mm.alzahrani@takamol.com.sa>
2022-03-24 13:43:56 +0300

    Primary btn style

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -7,10 +7,10 @@
             <p class="text-xl sm:text-2xl xl:text-4xl text-center md:text-left leading-6 xl:leading-10 text-gray-100 md:w-96 2xl:w-2/3" >Takamol Hub</p>
             <p class="mt-4 md:w-80 lg:w-2/3 xl:w-3/4 text-center md:text-left  text-base leading-normal text-gray-200" >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat</p>
             <div class="inline-flex">
-            <button class="mt-6 shrink-0 w-full md:w-auto mt-8 py-2 md:py-3 px-10 mr-4 flex justify-center duration-700  items-center text-base rounded bg-cyan-400 transition hover:-translate-y-1 hover:bg-gray-100 hover:text-gray-800 font-medium text-white">
+            <button class="mt-6 shrink-0 w-full md:w-auto mt-8 py-2 md:py-3 px-10 mr-4 flex justify-center duration-700  items-center text-base rounded bg-cyan-400 transition hover:-translate-y-1 hover:bg-cyan-400 hover:text-white font-medium text-white">
                 Publish api
             </button>
-            <button class="mt-6 shrink-0 w-full md:w-auto  lg:mt-8 py-2 md:py-3 px-10 flex justify-center duration-700  items-center text-base rounded border-2 border-white transition hover:-translate-y-1 hover:bg-gray-100 hover:text-gray-800 font-medium text-white">
+            <button class="mt-6 shrink-0 w-full md:w-auto  lg:mt-8 py-2 md:py-3 px-10 flex justify-center duration-700  items-center text-base rounded border-2 border-white transition hover:-translate-y-1 hover:bg-gray-100 hover:text-cyan-400 font-medium text-white">
                 Explore
             </button> 
             </div>  
