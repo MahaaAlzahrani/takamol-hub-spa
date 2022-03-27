43c57bf8921834fcf8281e6fdaf200eda65fa0bc
mahazz <mm.alzahrani@takamol.com.sa>
2022-03-27 14:30:59 +0300

    Add font style of popular heading

--- src/components/home/PopularApis.vue
+++ src/components/home/PopularApis.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="py-8 mx-16">
-    <h1>Popular APIs on the Takamol Hub</h1>
+  <section class="py-16 mx-16">
+    <h1 class="font-bold text-3xl">Popular APIs on the Takamol Hub</h1>
   </section>
 </template>
 
