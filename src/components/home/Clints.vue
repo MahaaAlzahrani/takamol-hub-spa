8a6b6a6f5110267160ddeb9a3ff84a616ca6bd9d
mahazz <mm.alzahrani@takamol.com.sa>
2022-03-27 12:36:52 +0300

    Add clints images

--- src/components/home/Clints.vue
+++ src/components/home/Clints.vue
@@ -1,13 +1,25 @@
 <template>
    	<!-- Clints section -->
 	<section class="py-8">
-		<div class="w-full p-6 flex flex-col flex-grow flex-shrink">
-			<div class="flex-1 p-8 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-				<div class="flex items-stretch">
-					<img class="lg:w-1/4 lg:h-1/4 px-7" src="@/assets/img/01.png" alt="" />
-					<img class="lg:w-1/6 lg:h-20 px-7" src="@/assets/img/02.png" alt="" />
-					<img class="lg:w-1/6 lg:h-16 px-7" src="@/assets/img/03.png" alt="" />
-					<img class="lg:w-1/6 lg:h-16 px-7" src="@/assets/img/04.png" alt="" />
+		<div class="w-full p-6 flex flex-col flex-grow flex-shrink h-60">
+			<div class="p-8 bg-white rounded-t overflow-hidden shadow">
+				<div class="flex items-start">
+					<!-- <img class="flex-1 lg:w-1/4 lg:h-1/4 px-7" src="@/assets/img/01.png" alt="" />
+					<img class="flex-1 lg:w-6 lg:h-20 px-7" src="@/assets/img/02.png" alt="" />
+					<img class="flex-1 lg:w-1/6 lg:h-16 px-7" src="@/assets/img/03.png" alt="" />
+					<img class="flex-1 lg:w-1/12 lg:h-16 px-7" src="@/assets/img/04.png" alt="" /> -->
+                    <div class="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:pb-10 pb-16 items-center">
+                    <img class="flex-1 lg:w-6 lg:h-32 px-7" src="@/assets/img/hrsd.svg" alt="" />
+                    </div>
+                    <div class="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:pb-10 pb-16 items-center">
+					<img class="flex-1 lg:w-6 lg:h-32 px-7" src="@/assets/img/hrsd.svg" alt="" />
+                    </div>
+                    <div class="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:pb-10 pb-16 items-center">
+					<img class="flex-1 lg:w-6 lg:h-32 px-7" src="@/assets/img/hrsd.svg" alt="" />
+                    </div>
+                    <div class="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:pb-10 pb-16 items-center">
+					<img class="flex-1 lg:w-6 lg:h-32 px-7" src="@/assets/img/hrsd.svg" alt="" />
+                    </div>
 				</div>
 			</div>
 		</div>
