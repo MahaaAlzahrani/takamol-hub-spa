1514d0b9d03776c357ae5d5bde5edf78b2e72c76
mahazz <mm.alzahrani@takamol.com.sa>
2022-03-27 15:34:16 +0300

    Add api provider logo

--- src/components/home/PopularApis.vue
+++ src/components/home/PopularApis.vue
@@ -8,9 +8,7 @@
           <div class="flex items-center mb-3">
               <!-- Api provider logo -->
             <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded from-[#2FE3DB] to-[#834FE8] bg-gradient-to-tr text-white flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-              </svg>
+              <img class="w-6 h-6" src="@/assets/img/mlsd-w-logo.png" alt="">
             </div>
             <h2 class="text-black text-lg title-font font-medium">Lorem ipsum</h2>
           </div>
@@ -24,10 +22,7 @@
           <div class="flex items-center mb-3">
               <!-- Api provider logo -->
             <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded from-[#2FE3DB] to-[#834FE8] bg-gradient-to-tr text-white flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                <circle cx="12" cy="7" r="4"></circle>
-              </svg>
+             <img class="w-6 h-6" src="@/assets/img/mlsd-w-logo.png" alt="">
             </div>
             <h2 class="text-black text-lg title-font font-medium">Lorem ipsum</h2>
           </div>
@@ -41,11 +36,7 @@
           <div class="flex items-center mb-3">
               <!-- Api provider logo -->
             <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded from-[#2FE3DB] to-[#834FE8] bg-gradient-to-tr text-white flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <circle cx="6" cy="6" r="3"></circle>
-                <circle cx="6" cy="18" r="3"></circle>
-                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-              </svg>
+             <img class="w-6 h-6" src="@/assets/img/mlsd-w-logo.png" alt="">
             </div>
             <h2 class="text-black text-lg title-font font-medium">Lorem ipsum</h2>
           </div>
