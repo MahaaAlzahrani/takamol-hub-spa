e15aa3657d2f782bbcc5d068f00ddfcd94a223ce
mahazz <mm.alzahrani@takamol.com.sa>
2022-03-27 10:02:07 +0300

    Add p-8 to clints card

--- src/components/home/Clints.vue
+++ src/components/home/Clints.vue
@@ -2,7 +2,7 @@
    	<!-- Clints section -->
 	<section class="py-8">
 		<div class="w-full p-6 flex flex-col flex-grow flex-shrink">
-			<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
+			<div class="flex-1 p-8 bg-white rounded-t rounded-b-none overflow-hidden shadow">
 				<div class="flex items-stretch">
 					<img class="lg:w-1/4 lg:h-1/4 px-7" src="@/assets/img/01.png" alt="" />
 					<img class="lg:w-1/6 lg:h-20 px-7" src="@/assets/img/02.png" alt="" />
@@ -10,7 +10,6 @@
 					<img class="lg:w-1/6 lg:h-16 px-7" src="@/assets/img/04.png" alt="" />
 				</div>
 			</div>
-			<!-- <img class="w-full" src="./assets/images/border_background.png"> -->
 		</div>
 	</section>
 </template>
