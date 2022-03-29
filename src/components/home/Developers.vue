bf0c91fd8fbf81053e545df402f85fedcd4bda39
mahazz <mm.alzahrani@takamol.com.sa>
2022-03-29 11:19:20 +0300

    Add image of developers section

--- src/components/home/Developers.vue
+++ src/components/home/Developers.vue
@@ -1,5 +1,31 @@
 <template>
-    
+      <section class="py-2 mx-16">
+		<div class="container px-5 py-12 mx-auto">
+			<div class="flex flex-wrap items-center">
+				<div class="w-5/6 sm:w-1/2 p-6">
+					<img class="sm:h-64 mx-auto" src="@/assets/img/developers-image.png" />
+				</div>
+
+				<div class="w-full sm:w-1/2 p-6 mt-6">
+					<div class="align-middle">
+						<h2 class="w-full small-heading w400 font-bold text-left text-primary-color">Designed for developers</h2>
+						<div class="w-full mb-4">
+							<div class="h-1 underline_header w-20 my-0 py-0"></div>
+						</div>
+						<h1 class="text-3xl text-gray-800 font-bold text-left leading-none mb-3">
+							Lorem ipsum dolor sit amet, consetetur sadipscing elitr
+						</h1>
+						<p class="text-gray-600 text-left mb-8">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+							at ipsum eu nunc commodo posuere et sit amet ligula.
+							<br />
+							<br />
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
@@ -8,6 +34,6 @@
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 
 </style>
